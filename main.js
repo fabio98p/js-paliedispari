e59fbf0af7bb4451ecba4d5a81705a3012f6d6c2
fabio98p <parisifabio98@gmail.com>
2021-03-08 17:16:55 +0100

    paliedispari v0.3 fine palindroma

--- main.js
+++ main.js
@@ -1,26 +1,17 @@
+//#region palindromo
+function startPalindroma() {
+	if (isPalindroma(document.getElementById("text").value)) {
+		document.getElementById("notPalindroma").innerHTML = "Questa parola è palindroma"
+	}
+	else{
+		document.getElementById("notPalindroma").innerHTML = "Questa parola è non palindroma"
+	}
+}
 function isPalindroma(parola) {
 	return parola = parola.split("").reverse().join("");
 }
-function startPalindroma() {
-	if (isPalindroma(document.getElementById("text").value)) {
-		console.log("hello");
-	}
-	else{
-		console.log("nope");
-	}
-}
+//#endregion
 
-
-
-
-
-
-
-
-
-
-
-
-	function startParidispari() {
+function startParidispari() {
 	
 }

a083c80e5488a7708bc42afdaac2cac2b3d2e652
fabio98p <parisifabio98@gmail.com>
2021-03-08 16:27:31 +0100

    paliedispari v0.2 meta palindromo

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-function startPalindromi(params) {
-	
+function isPalindroma(parola) {
+	return parola = parola.split("").reverse().join("");
 }
-function startParidispari(params) {
+function startPalindroma() {
+	if (isPalindroma(document.getElementById("text").value)) {
+		console.log("hello");
+	}
+	else{
+		console.log("nope");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+	function startParidispari() {
 	
 }
